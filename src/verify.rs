c2ce24811859b2540f8235ebb836a32d744e00a0
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2026-04-23 22:39:54 +0530

    Added logic to verify a new code against the hash

---
+++ src/verify.rs
@@ -0,0 +1,23 @@
+use crate::audit::AuditEntry;
+use crate::crypto;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+pub fn verify_script_execution(_script_path: &std::path::Path) -> std::io::Result<()> {
+    let rehash = crypto::compute_script_sha256(_script_path)?;
+    let file = File::open("audit.log")?;
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line?;
+        let entry: AuditEntry = serde_json::from_str(&line)?;
+
+        if rehash == entry.script_hash {
+            println!("Verified: executed at {}", entry.timestamp_iso8601);
+            return Ok(());
+        }
+    }
+
+    println!("NOT FOUND");
+    Ok(())
+}
